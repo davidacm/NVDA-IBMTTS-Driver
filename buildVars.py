47034597018f0d37aac6ecd775ce927c33d65c67
david-acm <dhf360@gmail.com>
2019-03-30 15:04:57 -0600

    update driver add-on version.

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""This is the IBMTTS synthesizer driver for NVDA."""),
 	# version
-	"addon_version" : "19.2b2",
+	"addon_version" : "19.2b3",
 	# Author(s)
 	"addon_author" : u"David CM <dhf360@gmail.com> and others",
 	# URL for the add-on documentation support
