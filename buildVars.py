019a42d2e4ecefd3194205cb67b0377b0d9adc0c
david-acm <dhf360@gmail.com>
2019-05-27 18:58:07 -0600

    version 19.5B1 update NVDA compatibility versions. update scons, return to the older sconstruct that generates html readmes.

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""This is the IBMTTS synthesizer driver for NVDA."""),
 	# version
-	"addon_version" : "19.5a1",
+	"addon_version" : "19.5B1",
 	# Author(s)
 	"addon_author" : u"David CM <dhf360@gmail.com> and others",
 	# URL for the add-on documentation support
@@ -29,7 +29,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0")
 	"addon_minimumNVDAVersion" : "2018.2.0",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2019.1.0",
+	"addon_lastTestedNVDAVersion" : "2019.2.0",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : None,
 }
