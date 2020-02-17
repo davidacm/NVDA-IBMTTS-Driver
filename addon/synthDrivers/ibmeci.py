56166f17e00a712bf6da6aa78694f0aaab07fc4a
ultrasound1372 <ultrasound2001@gmail.com>
2020-02-17 16:34:06 -0600

    updated h apostrophe expression for a use case I was sent this morning

--- addon/synthDrivers/ibmeci.py
+++ addon/synthDrivers/ibmeci.py
@@ -37,7 +37,7 @@ anticrash_res = {
 	re.compile(br"\b(|\d+|\W+)h'(r|v)[e]", re.I): br"\1h ' \2 e",
 	re.compile(br"\b(\w+[bdflmnrvzqh])hes([bcdfgjklmnprtw]\w+)\b", re.I): br"\1 hes\2",
 	re.compile(br"(\d):(\d\d[snrt][tdh])", re.I): br"\1 \2",
-	re.compile(br"h'([bdfjkpstvx']+)'([rtv][aeiou]?)", re.I): br"h \1 \2",
+	re.compile(br"h'([bdfjklpstvx']+)'([rtv][aeiou]?)", re.I): br"h \1 \2",
 	re.compile(br"(re|un|non|anti)cosp", re.I): br"\1kosp",
 	re.compile(br"(anti|non|re|un)caesure", re.I): br"\1ceasure",
 	re.compile(br"(EUR[A-Z]+)(\d+)", re.I): br"\1 \2",
